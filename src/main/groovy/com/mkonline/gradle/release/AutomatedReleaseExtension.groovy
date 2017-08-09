49e10cab9a7e871d1a5552839cd71583a5133b17
Artur Zagozdzinski <aza@markedskraft.com>
2017-08-09 13:40:45 +0200

    first release

---
+++ src/main/groovy/com/mkonline/gradle/release/AutomatedReleaseExtension.groovy
@@ -0,0 +1,8 @@
+package com.mkonline.gradle.release
+
+/**
+ * Created by artur on 09.08.2017.
+ */
+class AutomatedReleaseExtension {
+    String packagingType;
+}
---
+++ src/main/groovy/com/mkonline/gradle/release/AutomatedReleasePlugin.groovy
@@ -0,0 +1,35 @@
+package com.mkonline.gradle.release
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.GradleBuild
+
+
+/**
+ * Gradle plugin that provides custom tasks for interacting with Docker via its remote API.
+ * Gradle plugin that provides custom tasks
+ */
+class AutomatedReleasePlugin implements Plugin<Project> {
+
+    static final String RELEASE_GROUP = 'Release'
+
+    private AutomatedReleaseExtension extension
+    private Map<String, Object> attributes = [:]
+
+    @Override
+    void apply(Project project) {
+        extension = project.extensions.create('automateRelease', AutomatedReleaseExtension, project, attributes)
+
+        project.task('packageAndRelease', description: 'Verify project, release, and update version to next.',
+                group: RELEASE_GROUP) doLast this.&verifyVersion()
+    }
+
+    void verifyVersion() {
+        if ('MAVEN'.equals(extension.packagingType)) {
+            println '-- RUNNING MAVEN TYPE PKG --'
+        } else if ('DOCKER'.equals(extension.packagingType)) {
+            println '-- RUNNING DOCKER TYPE PKG --'
+        }
+    }
+
+}

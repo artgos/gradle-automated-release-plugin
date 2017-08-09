9ffee0023c13ca21cdc3d72d1b5a9d7b8bdbf4d9
Artur Zagozdzinski <aza@markedskraft.com>
2017-08-09 13:47:38 +0200

    Etension constructor added

--- src/main/groovy/com/mkonline/gradle/release/AutomatedReleaseExtension.groovy
+++ src/main/groovy/com/mkonline/gradle/release/AutomatedReleaseExtension.groovy
@@ -1,8 +1,22 @@
 package com.mkonline.gradle.release
 
+import org.gradle.api.Project
+
 /**
  * Created by artur on 09.08.2017.
  */
 class AutomatedReleaseExtension {
+
+    private Project project
+    private Map<String, Object> attributes
+
     String packagingType;
+
+    AutomatedReleaseExtension(Project project, Map<String, Object> attributes) {
+        this.attributes = attributes
+        this.project = project
+        ExpandoMetaClass mc = new ExpandoMetaClass(ReleaseExtension, false, true)
+        mc.initialize()
+        metaClass = mc
+    }
 }
